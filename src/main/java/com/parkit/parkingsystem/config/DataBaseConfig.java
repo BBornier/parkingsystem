ce895f0f492f47e9dca58659d23aebe66fc48771
Benoît B <benoitbornier@yahoo.fr>
2021-07-13 12:55:43 +0200

    Changement de mot de passe fort pour la connexion à la base de donnée.

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -13,7 +13,7 @@ public class DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","rootroot");
+                "jdbc:mysql://localhost:3306/prod","root","X8AHNdCXq&n6e&ugRkM+dTN2#H+!uJ!Fa9U85*G3We*7nn4DU*=+Dt4csgF!h?dX!XaMg-8CM5V5MV-pTEG7Q-#-Nns6D?N8bw^Frn+CxR-FjAbzL+Fty6=R%mT26gYXu7rN-$$8BGD&&b?^rm7Tje7HWDW8W=5CYyjY_ZWcDcgzVCymAj^KwgK^FHfb^w2BV96^h8kpsK!T=!eCt5U$Hxc=wTGrS@8yqjm@ngX&WE#QsYBj@26wdwsjLwAp&&@*");
     }
 
     public void closeConnection(Connection con){
