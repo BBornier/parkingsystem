487fa18513b63406394deec988e02a9534b88c34
Benoît B <benoitbornier@yahoo.fr>
2021-08-24 15:28:14 +0200

    ajout d'une classe de test pour TestInteractiveShell.java

---
+++ src/test/java/com/parkit/parkingsystem/integration/service/TestInteractiveShell.java
@@ -0,0 +1,14 @@
+package com.parkit.parkingsystem.integration.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestInteractiveShell {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
