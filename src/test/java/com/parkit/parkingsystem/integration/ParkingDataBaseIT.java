3669179280c76a67e8801beb57815435763ef158
Benoît B <benoitbornier@yahoo.fr>
2021-08-26 16:52:03 +0200

    Commencement d'écriture du test testParkingACar() à compléter; ajouts de commentaires pour expliciter quoi faire dans les 2 méthodes de test à compléter.

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.lang.System.Logger;
@@ -17,6 +18,7 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 
@@ -57,6 +59,13 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+        // vérifier qu'un ticket est bien enregistré en BDD :
+        TicketDAO ticketBdd = new TicketDAO();
+        Ticket ticket = null;
+		ticketBdd.saveTicket(ticket);
+		assertEquals(ticketBdd, ticket);
+		
+		// vérifier que la table Parking est bien mise à jour sur les disponibilités de places de parking : 
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
     }
 
@@ -65,6 +74,15 @@ public class ParkingDataBaseIT {
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
+        // Vérifier que le prix est bien généré et inscrit en BDD : 
+        
+        
+        
+        
+        // Vérifier que le temps de sortir est bien noté et inscrit en BDD :
+        
+        
+        
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
 
