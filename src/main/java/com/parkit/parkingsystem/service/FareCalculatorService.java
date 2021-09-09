ef6372535426019105217c9e16fd00da1cd25922
Benoît B <benoitbornier@yahoo.fr>
2021-09-09 18:26:19 +0200

    Essai creation méthode d'arrondit dans FareCalculatorService.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,38 +1,40 @@
 package com.parkit.parkingsystem.service;
 
+import java.text.DecimalFormat;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+    public void calculateFare(Ticket ticket) {
     	// Problème de calcul ici.
     	// Méthodes dépréciées. remplacées par getTime().
         //Appel de la méthode tout simplement avec son nom. 
         //On stocke les infos de la variable duration avec la méthode calculateTicketParkTime.
-        long duration = calculateTicketParkTime(ticket.getOutTime().getTime(), ticket.getInTime().getTime());
+        long duration = calculateTicketParkTime(ticket.getOutTime().getTime(), ticket.getInTime().getTime());      
         // @SuppressWarnings("deprecation")
 		// long inHour = ticket.getInTime().getTime();
         // @SuppressWarnings("deprecation")
 		// long outHour = ticket.getOutTime().getTime();
-
         // TODO: Some tests are failing here. Need to check if this logic is correct
         // long duration = outHour - inHour;
 
-		switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR)/60/60/1000);       
+		switch (ticket.getParkingSpot().getParkingType()){ // Attribue un type de place de parking (voiture ou vélo)
+            case CAR: { // Dans le cas d'une voiture qui se gare
+            	//Prend la durée de stationnement, multiplie par le prix du stationnement à l'heure (ici 1.5),
+            	//puis la durée en millisecondes est convertit en minutes pour avoir le bon résultat.
+                ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR)/(60*60*1000)); 
                 break;
             }
-            case BIKE: {
-                ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR)/60/60/1000);               
+            case BIKE: { // Dans le cas d'un vélo qui se gare
+                ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR)/(60*60*1000));               
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
           
         }
     }
-    // Type long avant la méthode :
     public long calculateTicketParkTime(long outHour, long inHour) {
     	
     	// réadaptation du if de départ.
@@ -43,11 +45,11 @@ public class FareCalculatorService {
 		return duration;	
 		
     }
-    // Ajout d'une méthode de calcul de 2 chiffres après la décimale :
-    //public void onlyTwoDecimalNumbers() {
-    	//FareCalculatorService fareCalc = new FareCalculatorService();
-    	//DecimalFormat df = new DecimalFormat("#.##");
-    	//fareCalc.calculateFare(Ticket);
-        //df.format(fareCalc);
-    //}
+    	// Ajout d'une méthode de calcul de 2 chiffres seulement après la décimale :
+    public void onlyTwoDecimalNumbers(long duration, Fare fare) {
+    	Ticket ticket = new Ticket();
+    	DecimalFormat df = new DecimalFormat("0.00");
+    	df.format(ticket);
+    	
+    }
 }
