3d7c0730bf62d31faaf5d3070758453e48ba5d7d
Benoît B <benoitbornier@yahoo.fr>
2021-09-23 12:40:43 +0200

    Modification du premier test d'intégration testParkingACar().

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,13 @@
 package com.parkit.parkingsystem.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.lang.System.Logger;
+import java.sql.SQLException;
+import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.AfterAll;
@@ -14,6 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.parkit.parkingsystem.config.DataBaseConfig;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -45,6 +50,8 @@ public class ParkingDataBaseIT {
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
+    	// Unnecessary stubbing detected.
+    	// org.mockito.exceptions.misusing.UnnecessaryStubbingException.
         when(inputReaderUtil.readSelection()).thenReturn(1);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         dataBasePrepareService.clearDataBaseEntries();
@@ -58,13 +65,17 @@ public class ParkingDataBaseIT {
     @Test
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
+        parkingService.processIncomingVehicle();        
         // vérifier qu'un ticket est bien enregistré en BDD :
-        TicketDAO ticketBdd = new TicketDAO();
-        Ticket ticket = null;
-		ticketBdd.saveTicket(ticket);
-		assertEquals(ticketBdd, ticket);
-		
+        Ticket ticket = new Ticket();
+        try {
+			dataBaseTestConfig.getConnection();
+			ticketDAO.getTicket("BA-788-QQ");
+			ticketDAO.saveTicket(ticket);
+			assertNotNull(ticket, "Le ticket n'est pas enregistré");
+        } catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 		// vérifier que la table Parking est bien mise à jour sur les disponibilités de places de parking : 
 		
 		
@@ -75,7 +86,6 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         // Vérifier que le prix est bien généré et inscrit en BDD : 
@@ -84,6 +94,7 @@ public class ParkingDataBaseIT {
         
         
         // Vérifier que le temps de sortir est bien noté et inscrit en BDD :
+        
         
         
         
