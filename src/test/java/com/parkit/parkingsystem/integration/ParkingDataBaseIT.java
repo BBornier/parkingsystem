afe21e03dd7a5e37cfc11c085186cf46e602e2b9
Benoit <benoi@SEPHIROTH-II.home>
2021-10-07 13:37:04 +0200

    Commentaires étape du test d'intégration d'enregistrement du prix en bdd.

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -109,55 +109,64 @@ public class ParkingDataBaseIT {
 	@Test
 	public void testParkingPriceIsWellGeneratedAndRegisteredInBdd_WhileA_CarIsExitingIT() {
 		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingService.processIncomingVehicle();
-		Ticket ticket = new Ticket();
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
-		Date outTime = new Date();
-		outTime.setTime(System.currentTimeMillis() + (60 * 60 * 1000));
-		ParkingSpot pkSpot = new ParkingSpot(2, ParkingType.CAR, false);
+		parkingService.processIncomingVehicle(); // Appel de la méthode gérant l'arrivée d'un véhicule.
+		Ticket ticket = new Ticket(); // instanciation d'un ticket.
+		Date inTime = new Date(); // instanciation d'un horaire (celui d'entrée du véhicule).
+		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000)); // appel de la méthode setTime() pour simuler l'horaire d'arrivée -1h par rapport à l'heure actuelle.
+		Date outTime = new Date(); // instanciation d'un horaire (celui de sortie du véhicule).
+		outTime.setTime(System.currentTimeMillis() + (60 * 60 * 1000)); // appel de la méthode setTime() pour simuler l'horaire de sortie +1h par rapport à l'heure actuelle.
+		ParkingSpot pkSpot = new ParkingSpot(2, ParkingType.CAR, false); // instanciation d'un objet pkSpot pour atttribuer une place de parking au véhicule.
 
-		ticket.setVehicleRegNumber("BA-788-QQ");
-		ticket.setParkingSpot(pkSpot);
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
-		fareCalculatorService.calculateFare(ticket);
+		ticket.setVehicleRegNumber("BA-788-QQ"); // appel de la méthode setVehiculeRegNumber pour attribuer une immat au véhicule qui rentre et l'attribuer à l'objet ticket.
+		ticket.setParkingSpot(pkSpot); // appel de la méthode setParkingSport pour attibuer une place de stationnement au véhicule qui rentre et l'attribuer à l'objet ticket.
+		ticket.setInTime(inTime); // appel de la méthode setInTime pour attibuer un horaire d'entrée au véhicule qui rentre et l'attribuer à l'objet ticket.
+		ticket.setOutTime(outTime); // appel de la méthode setOutTime pour attibuer un horaire de sortie au véhicule qui sort et l'attribuer à l'objet ticket.
+		fareCalculatorService.calculateFare(ticket); // utilisation de l'objet fareCalculatorService instancié dans le @ExtendWith et @BeforeAll. Il calcule le prix du ticket grâce aux méthodes et paramètres passés au ticket juste avant.
 		try {
-			dataBaseTestConfig.getConnection();
-			ticketDAO.saveTicket(ticket);
-			ticket = ticketDAO.getTicket("BA-788-QQ");
-			dataBaseTestConfig.closeConnection(null);
-			dataBaseTestConfig.getConnection();
-			ticketDAO.updateTicket(ticket);
-			ticket = ticketDAO.getTicket("BA-788-QQ");
-			assertNotNull(ticket.getPrice());
-			parkingService.processExitingVehicle();
+			dataBaseTestConfig.getConnection(); // connection à la base de données.
+			ticketDAO.saveTicket(ticket); // sauvegarde du ticket en base de données.
+			ticket = ticketDAO.getTicket("BA-788-QQ"); // stockage du ticket en base de données avec tous ses paramètres.
+			ticketDAO.updateTicket(ticket); // mise à jour du ticket avec ses nouvelles informations et notamment le calcul du temps passé et donc du prix.
+			ticket = ticketDAO.getTicket("BA-788-QQ"); // stockage de la mise à jour dans le ticket.
+			assertNotNull(ticket.getPrice()); // assertion du tests pour savoir si oui ou non le prix du ticket est null ou pas (vérification que le prix est bien enregistré en BDD).
+			parkingService.processExitingVehicle(); // appel de la méthode gérant la sortie du véhicule et la génération en console du tarif à payer.
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	// Vérifier que le temps de sortie est bien noté et inscrit en BDD > Test en
-	// cours. Mais test précédent fait le taf.
+	// Vérifier que le temps de sortie est bien noté et inscrit en BDD.
 	@Test
 	@Disabled
 	public void testOutTimeIsWellGeneratedAndRegisteredInBdd_WhileA_CarIsExitingIT() {
 		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingService.processExitingVehicle();
-		Ticket ticket = new Ticket();
-		Date inTime = new Date();
+		parkingService.processIncomingVehicle(); 
+		Ticket ticket = new Ticket(); 
+		Date inTime = new Date(); 
+		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000)); 
 		Date outTime = new Date();
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
-		ticket.setPrice(0);
+		outTime.setTime(System.currentTimeMillis() + (60 * 60 * 1000)); 
+		ParkingSpot pkSpot = new ParkingSpot(2, ParkingType.CAR, false);
+
+		ticket.setVehicleRegNumber("BA-788-QQ"); 
+		ticket.setParkingSpot(pkSpot); 
+		ticket.setInTime(inTime); 
+		ticket.setOutTime(outTime); 
+		fareCalculatorService.calculateFare(ticket); 
 		try {
-			dataBaseTestConfig.getConnection();
+			dataBaseTestConfig.getConnection(); 
+			ticketDAO.saveTicket(ticket); 
+			ticket = ticketDAO.getTicket("BA-788-QQ"); 
+			ticketDAO.updateTicket(ticket); 
+			ticket = ticketDAO.getTicket("BA-788-QQ");
+			parkingService.processExitingVehicle();
+			assertNotNull(ticket.getOutTime());
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
+	}
 
 		// TODO: check that the fare generated and out time are populated correctly in
 		// the database. DONE !
-	}
 
 }
