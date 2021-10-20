4e6d4ab7267ba3caed3fa7bc98fab305969fb287
Benoît B <benoitbornier@yahoo.fr>
2021-10-20 10:55:59 +0200

    Elaboration de la JavaDoc. Etape 1.

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -1,32 +1,63 @@
 package com.parkit.parkingsystem;
 
-import com.parkit.parkingsystem.service.InteractiveShell;
-
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.parkit.parkingsystem.service.InteractiveShell;
+
+/**
+ * <b>Park'it is an application created by Mov'it! to calculate parking tickets
+ * for cars and bikes.</b>
+ * <p>
+ * 
+ * @author BBornier
+ * @version 0.1
+ * 
+ */
+
 public class App {
-	// Appel de la signature de la méthode getLogger avec objet .class.
-    private static final Logger logger = LogManager.getLogger("App");
-    public static void main(String args[]){
-    	  
-        logger.info("Initializing Parking System");
-        InteractiveShell.loadInterface();
-        
-        logger.log(Level.INFO, "Logs with Log4J2");
-        //logger.log(Level.ERROR, "Houston, we have a problem");
-      
-        
-        logger.trace("Entering application");
-        App appParking = new App();
-        if (!appParking.doIt()) {
-        	logger.error("Didn't do it");
-        }
-        
-        logger.trace("Existing application");
-        
-    }
+	/**
+	 * <p>
+	 * The application is launched and propose to the custumer to enter with his car
+	 * or bike, write the vehicule register number. The application save in a
+	 * database the incoming hour, the vehicule register number. When the custumer
+	 * leave, the application ask for the vehicule register number and then show the
+	 * parking fare. Park'it register in the database the exiting hour and the price
+	 * to pay.
+	 * <p>
+	 * Here, Log4J has been set up to be used from the beginning at application
+	 * launch.
+	 * 
+	 * @see LogManager
+	 * @see getLogger
+	 * 
+	 */
+	private static final Logger logger = LogManager.getLogger("App");
+
+	public static void main(String args[]) {
+
+		logger.info("Initializing Parking System");
+		InteractiveShell.loadInterface();
+
+		logger.log(Level.INFO, "Logs with Log4J2");
+
+		logger.trace("Entering application");
+		App appParking = new App();
+		if (!appParking.doIt()) {
+			logger.error("Didn't do it");
+		}
+
+		logger.trace("Existing application");
+
+	}
+
+	/**
+	 * Method that returns false by default.
+	 * 
+	 * @see doIt
+	 * 
+	 */
 	private boolean doIt() {
 		return false;
 	}
--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -14,16 +14,48 @@ import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 
+/**
+ * TicketDAO is the class where the application save a ticket in database, get
+ * and update ticket informations.
+ *
+ * @author BBornier
+ * 
+ * @version 0.1
+ */
 public class TicketDAO {
 
 	private static final Logger logger = LogManager.getLogger("TicketDAO");
 
 	public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
+	/**
+	 * Save ticket informations as the parking spot number, the vehicle numberplate,
+	 * the price, the hour when the vehicle is entering in the parking, and the hour
+	 * when the vehicle is exiting from parking.
+	 * <p>
+	 * Corrections : variables correctly writen at the good lines. Ressources closed
+	 * and written at their right place, in the right order. A multicatch as been
+	 * written to see how to write one (exceptions must be written from specific to
+	 * general).
+	 * <p>
+	 * A tag "SuppressWarnings" was written in order to supress insignificant
+	 * warnings.
+	 * 
+	 * @See Ticket
+	 * @see PreparedStatement
+	 * @see ResultSet
+	 * @see ParkingType
+	 * @see Connection
+	 * @see DBConstants
+	 * @see DataBaseConfig
+	 * 
+	 * @param ticket is an instance of Ticket.
+	 * 
+	 * @return the execution of SQL statments.
+	 * 
+	 */
 	@SuppressWarnings("finally")
 	public boolean saveTicket(Ticket ticket) {
-		// Initialisation des variables remontées dans le bon emplacement.
-		// Fermetures mal placées sont placées dans le bloc finally.
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
@@ -35,8 +67,6 @@ public class TicketDAO {
 			ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
 			ps.setTimestamp(5, (ticket.getOutTime() == null) ? null : (new Timestamp(ticket.getOutTime().getTime())));
 			return ps.execute();
-			// Multi Catch. Aller du plus précis au moins précis. Argument plus précis que
-			// simplement Exception tout court.
 		} catch (IllegalArgumentException e) {
 			logger.error("Error fetching something", e);
 		} catch (Exception ex) {
@@ -48,10 +78,34 @@ public class TicketDAO {
 		}
 	}
 
+	/**
+	 * Get ticket informations (saved in database) as the parking spot number, the
+	 * vehicle numberplate, the price, the hour when the vehicle is entering in the
+	 * parking, and the hour when the vehicle is exiting from parking.
+	 * <p>
+	 * Corrections : variables correctly writen at the good lines. Ressources closed
+	 * and written at their right place, in the right order. A multicatch as been
+	 * written to see how to write one (exceptions must be written from specific to
+	 * general).
+	 * <p>
+	 * A tag "SuppressWarnings" was written in order to supress insignificant
+	 * warnings.
+	 * 
+	 * @See Ticket
+	 * @see PreparedStatement
+	 * @see ResultSet
+	 * @see ParkingType
+	 * @see Connection
+	 * @see DBConstants
+	 * @see DataBaseConfig
+	 * 
+	 * @param vehicleRegNumber is the numberplate of a car or a bike.
+	 * 
+	 * @return the ticket with all its informations.
+	 * 
+	 */
 	@SuppressWarnings("finally")
 	public Ticket getTicket(String vehicleRegNumber) {
-		// Initialisation des variables remontées dans le bon emplacement.
-		// Fermetures mal placées sont placées dans le bloc finally.
 		Connection con = null;
 		Ticket ticket = null;
 		PreparedStatement ps = null;
@@ -74,7 +128,6 @@ public class TicketDAO {
 		} catch (Exception ex) {
 			logger.error("Error fetching next available slot", ex);
 		} finally {
-			// Quoiqu'il se passe les lignes suivantes dans le finally seront executées.
 			dataBaseConfig.closeResultSet(rs);
 			dataBaseConfig.closePreparedStatement(ps);
 			dataBaseConfig.closeConnection(con);
@@ -82,8 +135,32 @@ public class TicketDAO {
 		}
 	}
 
+	/**
+	 * Update ticket informations and concerns only the price, the hour of the
+	 * exiting of the vehicle, and the Id of the ticket.
+	 * <p>
+	 * Corrections : variables correctly writen at the good lines. Ressources closed
+	 * and written at their right place, in the right order. A multicatch as been
+	 * written to see how to write one (exceptions must be written from specific to
+	 * general).
+	 * <p>
+	 * A tag "SuppressWarnings" was written in order to supress insignificant
+	 * warnings.
+	 * 
+	 * @See Ticket
+	 * @see PreparedStatement
+	 * @see ResultSet
+	 * @see ParkingType
+	 * @see Connection
+	 * @see DBConstants
+	 * @see DataBaseConfig
+	 * 
+	 * @param ticket is an instance of Ticket.
+	 * 
+	 * @return the execution of SQL statments.
+	 * 
+	 */
 	public boolean updateTicket(Ticket ticket) {
-		// Fermeture de la ressource de BDD pour PrepareStatement ps comme précédemment.
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
